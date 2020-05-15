5fae4a87753e6997dffd48006cf99f819f0e8bda
viznev <viznev@users.noreply.github.com>
2020-05-14 19:42:47 -0500

    first test

--- src/main/java/com/example/demo/controller/CarController.java
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Car;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
+    public Car add(Car car) {
+        return car;
+    }
 }
--- src/test/java/com/example/demo/controller/CarControllerTest.java
+++ src/test/java/com/example/demo/controller/CarControllerTest.java
@@ -1,4 +1,31 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Car;
+import com.example.demo.service.CarService;
+import org.apache.tomcat.util.http.fileupload.MultipartStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class CarControllerTest {
+    private CarController carController;
+    private CarService carService;
+
+    @BeforeEach
+    public void init() {
+        this.carController = new CarController();
+    }
+
+    @Test
+    public void givenValidCar_whenAddIsCalled_thenDoesNotReturnNull() {
+        // Arrange
+        Car car = new Car();
+
+        // Act
+        Car actual = carController.add(car);
+
+        // Assert
+        assertNotNull(actual);
+    }
 }
